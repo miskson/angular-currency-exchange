195dd3afb602975a34ab5ef83ef8612e55f95cc0
serhi00 <sergejlevsna@gmail.com>
2024-08-14 02:59:56 +0300

    fix: fixed onInit requests to get data firstly. fixed bug where equal currencies were not equal when converting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,18 @@ import { ExchangeHeaderComponent } from './components/exchange-header/exchange-h
 import { HttpClientModule } from '@angular/common/http';
 import { DataService } from './services/data/data.service';
 import { CurrencyInfo } from './interfaces/data';
+import { firstValueFrom } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ExchangeHeaderComponent, HttpClientModule],
+  imports: [
+    RouterOutlet,
+    ExchangeHeaderComponent,
+    HttpClientModule,
+    FormsModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
@@ -20,10 +27,10 @@ export class AppComponent implements OnInit {
   data: CurrencyInfo[] = [];
   dataUah: CurrencyInfo[] = [];
 
-  currentSellCurrency: string = '980';
-  currentBuyCurrency: string = '840';
-  sellValue?: number = 1;
-  buyValue?: number = 1;
+  currentSellCurrency: string = '840';
+  currentBuyCurrency: string = '980';
+  sellValue: number = 1;
+  buyValue: number = 1;
 
   onSellCurrencyChange(e: Event): void {
     this.currentSellCurrency = (e.target as HTMLInputElement).value;
@@ -35,98 +42,103 @@ export class AppComponent implements OnInit {
     this.convertCurrency('buy');
   }
 
-  onSellInputChange(e: Event): void {
-    this.sellValue = +(e.target as HTMLInputElement).value;
+  onSellInputChange(value: number): void {
+    // this.sellValue = +(e.target as HTMLInputElement).value;
+    this.sellValue = value;
     this.convertCurrency('sell');
   }
 
-  onBuyInputChange(e: Event): void {
-    this.buyValue = +(e.target as HTMLInputElement).value;
+  onBuyInputChange(value: number): void {
+    // this.buyValue = +(e.target as HTMLInputElement).value;
+    this.buyValue = value;
     this.convertCurrency('buy');
   }
 
   convertCurrency(operation: 'sell' | 'buy') {
     if (+this.currentBuyCurrency === +this.currentSellCurrency) {
-      const temp = this.sellValue;
-      this.sellValue = this.buyValue;
+      const temp = operation === 'sell' ? +this.sellValue : this.buyValue;
+      this.sellValue = temp;
       this.buyValue = temp;
+    } else {
+      console.log('ELSE BLOCK');
+      let currentHrnArr: CurrencyInfo[] = [];
+      let actualCurrency: CurrencyInfo | undefined;
+
+      // console.log(
+      //   'CURRENCY CONVERT',
+      //   operation,
+      //   'currentSellCurrency',
+      //   this.currentSellCurrency,
+      //   'currentBuyCurrency',
+      //   this.currentBuyCurrency
+      // );
+
+      if (
+        +this.currentSellCurrency === 980 ||
+        +this.currentBuyCurrency === 980
+      ) {
+        currentHrnArr = this.data.filter(
+          (currency) => currency.currencyCodeB === 980
+        );
+      }
+
+      if (operation === 'sell') {
+        console.log('sell');
+        console.log(
+          'currentHrnArr.length',
+          currentHrnArr.length,
+          currentHrnArr
+        );
+
+        if (currentHrnArr.length > 0) {
+          actualCurrency = currentHrnArr.find(
+            (currency) => +this.currentBuyCurrency === currency.currencyCodeA
+          );
+
+          this.buyValue =
+            (this.sellValue as number) / (actualCurrency?.rateSell as number);
+        } else {
+          actualCurrency = this.data.find(
+            (currency) => currency.currencyCodeB !== 980
+          );
+          this.buyValue =
+            (this.sellValue as number) / (actualCurrency?.rateSell as number);
+        }
+      } else if ('buy') {
+        console.log('buy');
+        if (currentHrnArr.length > 0) {
+          actualCurrency = currentHrnArr.find(
+            (currency) => +this.currentSellCurrency === currency.currencyCodeA
+          );
+
+          this.sellValue =
+            (this.buyValue as number) * (actualCurrency?.rateBuy as number);
+        } else {
+          actualCurrency = this.data.find(
+            (currency) => currency.currencyCodeB !== 980
+          );
+          this.sellValue =
+            (this.buyValue as number) * (actualCurrency?.rateBuy as number);
+        }
+      }
     }
+  }
 
-    let currentHrnArr: CurrencyInfo[] = [];
-    let actualCurrency: CurrencyInfo | undefined;
-
-    console.log(
-      'CURRENCY CONVERT',
-      operation,
-      'currentSellCurrency',
-      this.currentSellCurrency,
-      'currentBuyCurrency',
-      this.currentBuyCurrency
-    );
-
-    if (+this.currentSellCurrency === 980 || +this.currentBuyCurrency === 980) {
-      currentHrnArr = this.data.filter(
+  async getCurrencyData(): Promise<void> {
+    try {
+      const res = await firstValueFrom(this.dataService.getApiCurrencyData());
+      this.data = res.filter((currency) => !currency.rateCross);
+      this.dataUah = this.data.filter(
         (currency) => currency.currencyCodeB === 980
       );
-    }
-
-    if (operation === 'sell') {
-      console.log('sell');
-
-      console.log('currentHrnArr.length', currentHrnArr.length, currentHrnArr);
-      if (currentHrnArr.length > 0) {
-        actualCurrency = currentHrnArr.find(
-          (currency) => +this.currentBuyCurrency === currency.currencyCodeA
-        );
-
-        this.buyValue =
-          (this.sellValue as number) / (actualCurrency?.rateSell as number);
-      } else {
-        actualCurrency = this.data.find(
-          (currency) => currency.currencyCodeB !== 980
-        );
-        this.buyValue =
-          (this.sellValue as number) / (actualCurrency?.rateSell as number);
-      }
-    } else if ('buy') {
-      console.log('buy');
-      if (currentHrnArr.length > 0) {
-        actualCurrency = currentHrnArr.find(
-          (currency) => +this.currentBuyCurrency === currency.currencyCodeA
-        );
-
-        this.sellValue =
-          (this.buyValue as number) * (actualCurrency?.rateBuy as number);
-      } else {
-        actualCurrency = this.data.find(
-          (currency) => currency.currencyCodeB !== 980
-        );
-        this.sellValue =
-          (this.buyValue as number) * (actualCurrency?.rateBuy as number);
-      }
+      console.log('RES', this.data, this.dataUah);
+    } catch (err) {
+      console.error(err, 'Request error!');
     }
   }
 
-  getCurrencyData() {
-    this.dataService.getApiCurrencyData().subscribe(
-      (res) => {
-        this.data = res.filter((currency) => {
-          return currency.rateCross ? false : true;
-        });
-        this.dataUah = this.data.filter((currency) => {
-          return currency.currencyCodeB === 980;
-        });
-
-        console.log('RES', this.data, this.dataUah);
-      },
-      (err) => {
-        console.error(err, 'REQUES ERROR');
-      }
-    );
-  }
-
-  ngOnInit(): void {
-    this.getCurrencyData();
+  async ngOnInit(): Promise<void> {
+    await this.getCurrencyData();
     this.convertCurrency('sell');
   }
 }
